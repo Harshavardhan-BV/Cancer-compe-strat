aaa4aee7d4c9a7c8570c15d6387f12eb3f4aeae6
Harshavardhan-BV <harshavardhan.bv@students.iiserpune.ac.in>
2020-12-24 17:37:16 +0530

    pairwise T+ Tp

--- analysis/pairwise_Tpos-Tpro.py
+++ analysis/pairwise_Tpos-Tpro.py
@@ -14,4 +14,65 @@ cf.timeseries(pre_path=pre_path,parm_name=parm_name,parm_array=mu_test_arr,parm_
 df=cf.eq_values(pre_path=pre_path,parm_name=parm_name,parm_array=mu_test_arr,parm_format=parm_format)
 cf.eqvparm(df,pre_path=pre_path,parm_name=parm_name,plot_Tneg=False)
 
+# Changing u_lim_o2 with fixed l_lim_o2
+parm_format='{:.1f}'
+o2_lim_arr=np.empty([0,2])
+for ulim_Tpos in np.arange(0.1,1,0.2):
+    for ulim_Tpro in np.arange(0.1,1,0.2):
+        o2_lim_arr=np.append(o2_lim_arr,[[ulim_Tpos,ulim_Tpro]],axis=0)
+parm_name='u_lim_o2Tpos-u_lim_o2Tpro'
+parm_name_array=['u_lim_o2Tpos','u_lim_o2Tpro']
+cf.timeseries(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,plot_Tneg=False)
+df=cf.eq_values(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,parm_name_array=parm_name_array)
+df[parm_name_array[0]]=df[parm_name_array[0]].round(1)
+df[parm_name_array[1]]=df[parm_name_array[1]].round(1)
+cf.heatmap_eqvparm(df,pre_path=pre_path,parm_name=parm_name,parm_name_array=parm_name_array,plot_Tneg=False,shareaxis=True)
 
+# Changing u_lim_test with fixed l_lim_test
+parm_name='u_lim_testTpos-u_lim_testTpro'
+parm_name_array=['u_lim_testTpos','u_lim_testTpro']
+cf.timeseries(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,plot_Tneg=False)
+df=cf.eq_values(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,parm_name_array=parm_name_array)
+df[parm_name_array[0]]=df[parm_name_array[0]].round(1)
+df[parm_name_array[1]]=df[parm_name_array[1]].round(1)
+cf.heatmap_eqvparm(df,pre_path=pre_path,parm_name=parm_name,parm_name_array=parm_name_array,plot_Tneg=False,shareaxis=True)
+
+# Changing l_lim_o2 with fixed u_lim_o2
+o2_lim_arr=np.empty([0,2])
+for llim_Tpos in np.arange(0,1,0.2):
+    for llim_Tpro in np.arange(0,1,0.2):
+        o2_lim_arr=np.append(o2_lim_arr,[[llim_Tpos,llim_Tpro]],axis=0)
+parm_name='l_lim_o2Tpos-l_lim_o2Tpro'
+parm_name_array=['l_lim_o2Tpos','l_lim_o2Tpro']
+cf.timeseries(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,plot_Tneg=False)
+df=cf.eq_values(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,parm_name_array=parm_name_array)
+df[parm_name_array[0]]=df[parm_name_array[0]].round(1)
+df[parm_name_array[1]]=df[parm_name_array[1]].round(1)
+cf.heatmap_eqvparm(df,pre_path=pre_path,parm_name=parm_name,parm_name_array=parm_name_array,plot_Tneg=False,shareaxis=True)
+
+# Changing l_lim_test with fixed u_lim_test
+parm_name='l_lim_testTpos-l_lim_testTpro'
+parm_name_array=['l_lim_testTpos','l_lim_testTpro']
+cf.timeseries(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,plot_Tneg=False)
+df=cf.eq_values(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,parm_name_array=parm_name_array)
+df[parm_name_array[0]]=df[parm_name_array[0]].round(1)
+df[parm_name_array[1]]=df[parm_name_array[1]].round(1)
+cf.heatmap_eqvparm(df,pre_path=pre_path,parm_name=parm_name,parm_name_array=parm_name_array,plot_Tneg=False,shareaxis=True)
+
+# Changing l_lim_o2 & u_lim_o2 by same amount (fixed difference)
+parm_name='cs_lim_o2Tpos-cs_lim_o2Tpro'
+parm_name_array=['l_lim_o2Tpos','l_lim_o2Tpro']
+cf.timeseries(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,plot_Tneg=False)
+df=cf.eq_values(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,parm_name_array=parm_name_array)
+df[parm_name_array[0]]=df[parm_name_array[0]].round(1)
+df[parm_name_array[1]]=df[parm_name_array[1]].round(1)
+cf.heatmap_eqvparm(df,pre_path=pre_path,parm_name=parm_name,parm_name_array=parm_name_array,plot_Tneg=False,shareaxis=True)
+
+# Changing l_lim_test & u_lim_test by same amount (fixed difference)
+parm_name='cs_lim_testTpos-cs_lim_testTpro'
+parm_name_array=['l_lim_testTpos','l_lim_testTpro']
+cf.timeseries(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,plot_Tneg=False)
+df=cf.eq_values(pre_path=pre_path,parm_name=parm_name,parm_array=o2_lim_arr,parm_format=parm_format,parm_name_array=parm_name_array)
+df[parm_name_array[0]]=df[parm_name_array[0]].round(1)
+df[parm_name_array[1]]=df[parm_name_array[1]].round(1)
+cf.heatmap_eqvparm(df,pre_path=pre_path,parm_name=parm_name,parm_name_array=parm_name_array,plot_Tneg=False,shareaxis=True)
---
+++ input/EnvEq/pairwise/Tpos-Tpro/u_lim_o2Tpos-u_lim_o2Tpro/input.py
@@ -0,0 +1,66 @@
+##--Enter all the input variables here--##
+#Refer doc/EnvEq_parms for further info
+
+#Time parameters of simulation
+t_max = 1000*24*60 #maximum (mins)
+dt = 60  #step (mins)
+#Initial Values
+##cells
+y0_Tpos=1000
+y0_Tpro=1000
+y0_Tneg=0
+##resource
+y0_o2=0.5 #(prop)
+y0_test=0 #(prop)
+#Production rates of resources
+p_o2=0.11 #(prop/min)
+p_test=5E-7 #by Tp cells (prop/min/cell)
+#Uptake rate of resources by cells
+##oxygen
+mu_o2Tpos=1.63E-6 #(prop/min/cell)
+mu_o2Tpro=1.63E-6 #(prop/min/cell)
+mu_o2Tneg=1.04E-6 #(prop/min/cell)
+##testosterone
+mu_testTpos=6E-8 #(prop/min/cell)
+mu_testTpro=2.34E-8 #(prop/min/cell)
+#Decay rates of resources
+lam_o2=0.1 #(1/min)
+lam_test=0.004 #(1/min)
+#Doubling time
+t_DTpos=34*60 #(min)
+t_DTpro=40*60 #(min)
+t_DTneg=25*60 #(min)
+#Growth rate
+r_Tpos= 2.84E-3 #(/min)
+r_Tpro= 2.79E-3 #(/min)
+r_Tneg= 6.23E-4 #(/min)
+#Death rate
+delta_Tpos= 2.5E-3 #(/min)
+delta_Tpro= 2.5E-3 #(/min)
+delta_Tneg= 1.6E-4 #(/min)
+#Min Carrying capacity
+K=1
+#Environmental Carrying capacity (Scaling Factor)
+rho_Tpos=8.35E4
+rho_Tpro=9.62E4
+rho_Tneg=1.34E4
+#Resource limits
+##Oxygen
+###T+
+l_lim_o2Tpos=0 #lower-threshold(prop)
+u_lim_o2Tpos=1 #upper-saturation(prop)
+###Tp
+l_lim_o2Tpro=0 #(prop)
+u_lim_o2Tpro=1 #(prop)
+###T-
+l_lim_o2Tneg=0 #(prop)
+u_lim_o2Tneg=1 #(prop)
+##Testosterone
+###T+
+l_lim_testTpos=0 #(prop)
+u_lim_testTpos=1 #(prop)
+###Tp
+l_lim_testTpro=0 #(prop)
+u_lim_testTpro=1 #(prop)
+#filename to save output in
+f_name='pairwise/Tpos-Tpro/u_lim_o2Tpos-u_lim_o2Tpro/'
